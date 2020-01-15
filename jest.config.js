75fe5ed5665fe7c463dd1095fbac2a67fbc77b35
Daniil Ryazanov <kein@tagproject.ru>
2020-01-15 23:41:40 +0300

    workflow(jest): fix configs

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
     testEnvironment: 'node',
     collectCoverage: true,
     coverageReporters: ['text-summary', 'json'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test))\\.(tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testPathIgnorePatterns: ['/lib/', '/node_modules/'],
     globals: {
         'ts-jest': {
             babelConfig: {
